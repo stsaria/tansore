6a00dfd6e23e966967b8f661c8431d1621c803a8
stsaria <solothunder.autoer@gmail.com>
2023-10-19 14:15:00 +0900

    ububub

--- tansore.py
+++ tansore.py
@@ -1,10 +1,10 @@
 import sys
 from gui import *
-from install import *
 
 if __name__ == "__main__":
     if len(sys.argv) >= 2:
         if sys.argv[1] == "-install":
+            from install import *
             install_tansore()
         else:
             print("I'm not sure about this argument.......")
